61e4a925b7b1ed3b495e3cfba8d6cf490f318603
Your Name <Sajid.Khan@paccar.com>
2024-12-09 22:02:41 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductService } from './services/product.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule // Add HttpClientModule here
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/common/product.ts
@@ -0,0 +1,17 @@
+export class Product {
+    constructor (public sku: string,
+        public name : string,
+        public description : string,
+        public unitPrice : string,
+        public imageUrl : string,
+        public active : string,
+        public  dateCreated : Date,
+        public  lastCreated : Date,
+
+
+
+    ){
+
+    }
+
+}
